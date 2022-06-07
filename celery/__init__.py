73beec5c5e3d12188f4628fec6a75a79da3b5372
yaron <yaron.reshef@perception-point.io>
2022-06-07 16:20:11 +0300

    versions

--- celery/__init__.py
+++ celery/__init__.py
@@ -14,7 +14,7 @@ from collections import namedtuple
 
 SERIES = 'rhubarb'
 
-__version__ = '4.3.1'
+__version__ = '4.3.p'
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
 __homepage__ = 'http://celeryproject.org'
